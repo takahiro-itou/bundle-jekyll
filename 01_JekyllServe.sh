d5ea1021564339486b0531888ce5c278ab889448
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-13 18:55:26 +0900

    ローカルでサーバーを起動するシェルスクリプトを実装

---
+++ 01_JekyllServe.sh
@@ -0,0 +1,11 @@
+#!/bin/bash  -xue
+
+script_dir=$(readlink -f "$(dirname "$0")")
+
+blog_source_dir=$1
+host_accept_from=${2:-'0.0.0.0'}
+
+bundle exec jekyll serve    \
+    --source "${blog_source_dir}"   \
+    --host "${host_accept_from}"    \
+    ;
